b93df6fa34e1525f00317996d3ed966a4fef7230
Morozov <Morozov@0cb35ee9-c86f-ff42-8671-8322cad9788f>
2009-07-25 12:09:31 +0000

    Для терминала Windows переводим вывод в cp866.
    git-svn-id: svn://localhost/splitter/trunk@74 0cb35ee9-c86f-ff42-8671-8322cad9788f

--- _classes/Splitter/Response/Cli.php
+++ _classes/Splitter/Response/Cli.php
@@ -8,6 +8,20 @@
 class Splitter_Response_Cli extends Splitter_Response_Abstract {
 
 	/**
+	 * Windows ли?
+	 *
+	 * @var boolean
+	 */
+	protected $is_windows;
+
+	/**
+	 * Конструктор.
+	 */
+	public function __construct() {
+		$this->is_windows = Application::isWindows();
+	}
+
+	/**
 	 * Проекция типов сообщений на знаки, которыми они помечаются в журнале.
 	 *
 	 * @var array
@@ -30,7 +44,9 @@ class Splitter_Response_Cli extends Splitter_Response_Abstract {
 		$sign = isset(self::$types[$type]) ? self::$types[$type] : ' ';
 		$date = $this->getDate();
 		foreach (preg_split('/[\r\n]+/', $message) as $line) {
-			printf('%s | %s | %s' . PHP_EOL, $sign , $date, $line);
+			$this->write(
+				sprintf('%s | %s | %s' . PHP_EOL, $sign , $date, $line)
+			);
 			if ($first) {
 				$sign = ' ';
 				$date = str_repeat(' ', strlen($date));
@@ -47,6 +63,22 @@ class Splitter_Response_Cli extends Splitter_Response_Abstract {
 	 * @param array $arguments
 	 */
 	public function __call($method, array $arguments) {
-		printf('%s(%s)' . PHP_EOL, $method, implode(',', array_map('json_encode', $arguments)));
+		$this->write(
+			sprintf('%s(%s)' . PHP_EOL, $method, implode(',', array_map('json_encode', $arguments)))
+		);
+	}
+
+	/**
+	 * Выводит сообщение в терминал. Попутно преобразует кодировку для терминала
+	 * Windows. Использование обрабочика буфера вывода здесь не подходит, т.к.
+	 * вывод должен быть небуферизованным.
+	 *
+	 * @param string $message
+	 */
+	protected function write($message) {
+		if ($this->is_windows) {
+			$message = mb_convert_encoding($message, 'cp866', 'utf-8');
+		}
+		echo $message;
 	}
 }
